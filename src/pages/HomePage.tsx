e55806c6c45721f33a921edf420f9b1c8a9a393d
amra-mustajbasic <amra.mustajbasic@saltsquare.io>
2024-10-21 14:06:14 +0200

    Add Home Page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import homeBackground from '../assets/images/homeBackground.jpg';
+import { Box } from '@mui/material';
 
 const HomePage: React.FC = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <Box
+      sx={{
+        width: '100vw',
+        height: '100vh',
+        backgroundImage: `url(${homeBackground})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+      }}></Box>
+  );
 };
 
 export default HomePage;
