98ebd41471949d43d2300a2be073fb91324100c1
amra-mustajbasic <amra.mustajbasic@saltsquare.io>
2024-11-01 11:56:09 +0100

    Unknown page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegistrationPage from './pages/RegistrationPage';
 import RootLayout from './pages/Root';
 import HomePage from './pages/HomePage';
 import ProfilePage from './pages/ProfilePage';
 import ProtectedRoute from './components/ProtectedRoute';
+import LostPage from './pages/LostPage';
 
 const router = createBrowserRouter([
   {
@@ -29,6 +30,14 @@ const router = createBrowserRouter([
   {
     path: '/register',
     element: <RegistrationPage />
+  },
+  {
+    path: '/are-you-lost',
+    element: <LostPage />
+  },
+  {
+    path: '*',
+    element: <Navigate to="/are-you-lost" replace />
   }
 ]);
 
---
+++ src/pages/LostPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LostPage: React.FC = () => {
+  return <h1>Lost Page</h1>;
+};
+
+export default LostPage;
