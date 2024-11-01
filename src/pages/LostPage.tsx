d07784f9adfe9e905ff062f4fe31f531740bbd3c
amra-mustajbasic <amra.mustajbasic@saltsquare.io>
2024-11-01 14:44:35 +0100

    Implement custom are-you-lost page with embedded video player

--- src/pages/LostPage.tsx
+++ src/pages/LostPage.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import BackgroundVideo from '../components/BackgroundVideo';
+import { Box, Typography } from '@mui/material';
 
 const LostPage: React.FC = () => {
-  return <h1>Lost Page</h1>;
+  return (
+    <Box
+      sx={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: 'primary.main'
+      }}>
+      <Typography
+        variant="h2"
+        sx={{
+          position: 'absolute',
+          top: '3rem',
+          textShadow: '0.125rem 0.125rem 0.5rem rgba(0, 0, 0, 0.4)',
+          fontWeight: 'bold',
+          fontSize: '2.5rem'
+        }}>
+        Oops, are you lost?
+      </Typography>
+      <BackgroundVideo />
+    </Box>
+  );
 };
 
 export default LostPage;
