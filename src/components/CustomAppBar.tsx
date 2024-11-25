1afb364de8425d4aa379e9b3cbc48151f32fbcb3
amra-mustajbasic <amra.mustajbasic@saltsquare.io>
2024-11-25 20:23:51 +0100

    Logout functionality (#17)
    * Logout functionality
    
    * Implement logout functionality
    
    * Add error modal display for logout failures
    
    * Add function to handle logout error to avoid duplicate code

--- src/components/CustomAppBar.tsx
+++ src/components/CustomAppBar.tsx
@@ -1,45 +1,89 @@
 import { AppBar, Button, Toolbar } from '@mui/material';
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import HomeTwoToneIcon from '@mui/icons-material/HomeTwoTone';
-import React from 'react';
+import React, { useState } from 'react';
 import AccountCircleTwoToneIcon from '@mui/icons-material/AccountCircleTwoTone';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { logoutUser } from '../api/auth';
+import ErrorModal from './ErrorModal';
 
 const CustomAppBar: React.FC = () => {
+  const navigate = useNavigate();
+  const [isErrorModalOpen, setIsErrorModalOpen] = useState<boolean>(false);
+  const [errorDescription, setErrorDescription] = useState<string | null>(null);
+
+  const handleError = () => {
+    setErrorDescription('Something went wrong, please try again!');
+    setIsErrorModalOpen(true);
+  };
+
+  const handleLogout = async () => {
+    try {
+      const response = await logoutUser();
+      if (response.ok) {
+        navigate('/login');
+      } else {
+        handleError();
+      }
+    } catch (error) {
+      handleError();
+    }
+  };
+
+  const closeErrorModal = () => {
+    setIsErrorModalOpen(false);
+    setErrorDescription(null);
+  };
+
   return (
-    <AppBar
-      position="fixed"
-      elevation={0}
-      sx={{
-        backgroundColor: 'transparent',
-        boxShadow: 'none',
-        padding: '1rem',
-        top: 0,
-        right: 0
-      }}>
-      <Toolbar
+    <>
+      <AppBar
+        position="fixed"
+        elevation={0}
         sx={{
-          display: 'flex',
-          justifyContent: 'flex-end',
-          gap: '1rem'
+          backgroundColor: 'transparent',
+          boxShadow: 'none',
+          padding: '1rem',
+          top: 0,
+          right: 0
         }}>
-        <Button
-          component={RouterLink}
-          to="/"
-          className="appbar-button"
-          variant="text"
-          startIcon={<HomeTwoToneIcon />}>
-          Home
-        </Button>
-        <Button
-          component={RouterLink}
-          to="/profile"
-          className="appbar-button"
-          variant="text"
-          startIcon={<AccountCircleTwoToneIcon />}>
-          User Profile
-        </Button>
-      </Toolbar>
-    </AppBar>
+        <Toolbar
+          sx={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+            gap: '1rem'
+          }}>
+          <Button
+            component={RouterLink}
+            to="/"
+            className="appbar-button"
+            variant="text"
+            startIcon={<HomeTwoToneIcon />}>
+            Home
+          </Button>
+          <Button
+            component={RouterLink}
+            to="/profile"
+            className="appbar-button"
+            variant="text"
+            startIcon={<AccountCircleTwoToneIcon />}>
+            User Profile
+          </Button>
+          <Button
+            onClick={handleLogout}
+            className="appbar-button"
+            variant="text"
+            startIcon={<LogoutIcon />}>
+            Logout
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <ErrorModal
+        isOpen={isErrorModalOpen}
+        description={errorDescription}
+        onClose={closeErrorModal}
+      />
+    </>
   );
 };
 
